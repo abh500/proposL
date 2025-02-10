953ac3b4c02755d04afa2a5d128473a1992abda4
abh500 <71334742+abh500@users.noreply.github.com>
2025-02-10 15:06:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.getElementById('no-btn').addEventListener('click', function() {
+    document.getElementById('no-response').classList.remove('hidden');
+});
+
+document.getElementById('yes-btn').addEventListener('click', function() {
+    window.location.href = "next_page.html"; // This will lead to the next page
+});
 
